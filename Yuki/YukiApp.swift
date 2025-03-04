11b8d757d7be529109cff9080799ad9f953d6ba3
daniel <daniel.inama02@gmail.com>
2025-03-04 09:59:01 +0100

    Initial Commit

---
+++ Yuki/YukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YukiApp.swift
+//  Yuki
+//
+//  Created by Daniel Inama on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YukiTests/YukiTests.swift
@@ -0,0 +1,17 @@
+//
+//  YukiTests.swift
+//  YukiTests
+//
+//  Created by Daniel Inama on 4/3/25.
+//
+
+import Testing
+@testable import Yuki
+
+struct YukiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
